cf582969833e5779303529a42f4a15db6da677c4
Felipe Skinner <felipeskinner@gmail.com>
2015-10-07 13:21:21 -0300

    Corrige reducer do filtro de organizacao

--- client/src/reducers/shop.js
+++ client/src/reducers/shop.js
@@ -1,22 +1,26 @@
 import { ORG_REQUEST, ORG_SUCCESS, ORG_FAILURE } from '../constants/ActionTypes';
 
 const initialState = {
-  loading: false,
   developers: [
     {id: 1041, username: 'joao', price: 33.20},
     {id: 20073, username: 'jorge', price: 41.75},
     {id: 2248, username: 'pedro', price: 32},
     {id: 4660, username: 'paulo', price: 97.10}
   ],
+  loading: false,
   error: ''
 };
 
 export default function shop(state = initialState, action) {
+  const { response } = action;
+
   switch (action.type) {
   case ORG_REQUEST:
     return Object.assign({}, state, { loading: true, error: '' });
+
   case ORG_SUCCESS:
-    return Object.assign({}, state, { loading: false, developers: action.response.developers });
+    return Object.assign({}, state, { loading: false, developers: response.payload.developers });
+
   case ORG_FAILURE:
     return Object.assign({}, state, { loading: false, error: action.error });
 
